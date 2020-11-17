3e93b32d9d7aeb4a19093075a9c09484533e6182
Anton Ustinoff <a.a.ustinoff@gmail.com>
2020-11-17 12:07:47 +0400

    fix:(ui-kit) remove default icon from block empty & fix styles

--- components/blocks/BlockEmpty/index.js
+++ components/blocks/BlockEmpty/index.js
@@ -9,7 +9,7 @@ export default {
     props: {
         icon: {
             type: String,
-            default: 'user',
+            default: '',
         },
         title: {
             type: String,
