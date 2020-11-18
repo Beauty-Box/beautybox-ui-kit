1bea7cedb94aec63e7e35f2a5597ad8cb816df8d
Anton Ustinoff <a.a.ustinoff@gmail.com>
2020-11-18 14:26:40 +0400

    fix:(ui-kit) BtnClose icon

--- components/buttons/BtnClose/index.js
+++ components/buttons/BtnClose/index.js
@@ -50,14 +50,14 @@ export default {
                             size: this.size,
                         },
                     },
-                    'close--bold'
+                    'close'
                 )
             );
         } else {
             children.push(
                 h('b-svg', {
                     props: {
-                        name: 'close',
+                        name: 'close--bold',
                     },
                     attrs: {
                         style: { width: this.size / 2, height: this.size / 2 },
