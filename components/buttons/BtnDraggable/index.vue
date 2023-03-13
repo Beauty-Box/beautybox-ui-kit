61eef002fef0256124068f465aeeac9a0bd1b889
Brix-D <Shlyakhta4@gmail.com>
2023-03-13 17:59:25 +0400

    rewrote btn draggable to vue component

---
+++ components/buttons/BtnDraggable/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-btn
+        :type="type"
+        icon
+        large
+        :ripple="false"
+        class="c-btn-draggable"
+        :class="[`${color}--text`]"
+        v-on="$listeners"
+    >
+        <v-icon :size="iconSize" :color="color">
+            {{ icon }}
+        </v-icon>
+    </v-btn>
+</template>
+
+<script>
+import VBtn from 'vuetify/lib/components/VBtn';
+import VIcon from 'vuetify/lib/components/VIcon';
+
+export default {
+    name: 'BBtnDraggable',
+    components: { VBtn, VIcon },
+    props: {
+        type: {
+            type: String,
+            default: 'button',
+        },
+        color: {
+            type: String,
+            default: 'secondary',
+        },
+        icon: {
+            type: String,
+            default: 'menu',
+        },
+        iconSize: {
+            type: [String, Number],
+            default: 22,
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss" src="./BtnDraggable.scss"></style>
