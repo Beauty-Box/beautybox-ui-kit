0df99370d0ad3415324ed6c69719ae341ff49487
Brix-D <Shlyakhta4@gmail.com>
2023-03-27 15:34:45 +0400

    debug

--- components/icons/BSvg.vue
+++ components/icons/BSvg.vue
@@ -95,10 +95,10 @@ export default defineComponent({
 
     setup(props, { emit }) {
         const modules: Record<string, AsyncComponentLoader> = import.meta.glob(
-            '../../assets/spriteSVG/*.svg'
+            '../../assets/spriteSVG/*.svg?component'
         );
         const component = computed(() => {
-            const icon = modules[`../../assets/spriteSVG/${props.name}.svg?component`];
+            const icon = modules[`../../assets/spriteSVG/${props.name}.svg`];
             console.log('icon', icon);
             return defineAsyncComponent(icon);
         });
