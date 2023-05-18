59d552a459555c5dd5c48721c0d05982326b1ece
Brix-D <Shlyakhta4@gmail.com>
2023-05-18 14:06:37 +0400

    fix(vuetify table): try to fix vuetify table

--- components/tables/BTableLazyLoad.vue
+++ components/tables/BTableLazyLoad.vue
@@ -1,75 +1,105 @@
 <template>
-    <v-simple-table :fixed-header="fixedHeader" :height="height">
-        <template #default>
-            <thead v-if="showHeaderAll || !$vuetify.breakpoint.mobile">
-                <slot name="table-head" />
-            </thead>
-            <component
-                :is="tableBody"
-                v-if="useIntersection"
-                tag="tbody"
-                v-bind="draggableProps"
-                v-on="draggableListeners"
-            >
-                <slot v-if="$slots['table-body']" name="table-body" />
-                <template #footer>
-                    <tr v-if="loading">
-                        <td colspan="100%" align="center" :style="{ position: 'relative' }">
-                            <b-block-loader position="static" size="30" style="max-height: 60px" />
-                        </td>
-                    </tr>
-                    <tr v-else-if="nowItemsLength < allItemsLength">
-                        <td
-                            v-intersect.quiet="onIntersectBottom"
-                            colspan="100%"
-                            align="center"
-                            :style="{ position: 'relative', height: 'auto' }"
-                        />
-                    </tr>
-                </template>
-                <template v-if="!draggable">
-                    <tr v-if="loading">
-                        <td colspan="100%" align="center" :style="{ position: 'relative' }">
-                            <b-block-loader position="static" size="30" style="max-height: 60px" />
-                        </td>
-                    </tr>
-                    <tr v-else-if="nowItemsLength < allItemsLength">
-                        <td
-                            v-intersect.quiet="onIntersectBottom"
-                            colspan="100%"
-                            align="center"
-                            :style="{ position: 'relative', height: 'auto' }"
-                        />
-                    </tr>
-                </template>
-            </component>
-            <!-- старая реализация подгрузки по скроллу, по возможности убрать-->
-            <component
-                :is="tableBody"
-                v-else
-                v-scroll:#scroll-container="onScrollControl"
-                tag="tbody"
-                v-bind="draggableProps"
-                v-on="draggableListeners"
-            >
-                <slot v-if="$slots['table-body']" name="table-body" />
-                <template #footer>
-                    <tr v-if="loading">
-                        <td colspan="100%" align="center" :style="{ position: 'relative' }">
-                            <b-block-loader position="static" size="30" style="max-height: 60px" />
-                        </td>
-                    </tr>
-                </template>
-                <template v-if="!draggable">
-                    <tr v-if="loading">
-                        <td colspan="100%" align="center" :style="{ position: 'relative' }">
-                            <b-block-loader position="static" size="30" style="max-height: 60px" />
-                        </td>
-                    </tr>
-                </template>
-            </component>
-        </template>
-    </v-simple-table>
+    <div>
+        <v-simple-table :fixed-header="fixedHeader" :height="height">
+            <template #default>
+                <thead v-if="showHeaderAll || !$vuetify.breakpoint.mobile">
+                    <slot name="table-head" />
+                </thead>
+                <component
+                    :is="tableBody"
+                    v-if="useIntersection"
+                    tag="tbody"
+                    v-bind="draggableProps"
+                    v-on="draggableListeners"
+                >
+                    <slot v-if="$slots['table-body']" name="table-body" />
+                    <!-- <template #footer>
+                        <tr v-if="loading">
+                            <td colspan="100%" align="center" :style="{ position: 'relative' }">
+                                <b-block-loader
+                                    position="static"
+                                    size="30"
+                                    style="max-height: 60px"
+                                />
+                            </td>
+                        </tr>
+                        <tr v-else-if="nowItemsLength < allItemsLength">
+                            <td
+                                v-intersect.quiet="onIntersectBottom"
+                                colspan="100%"
+                                align="center"
+                                :style="{ position: 'relative', height: 'auto' }"
+                            />
+                        </tr>
+                    </template> -->
+                    <!-- <template v-if="!draggable">
+                        <tr v-if="loading">
+                            <td colspan="100%" align="center" :style="{ position: 'relative' }">
+                                <b-block-loader
+                                    position="static"
+                                    size="30"
+                                    style="max-height: 60px"
+                                />
+                            </td>
+                        </tr>
+                        <tr v-else-if="nowItemsLength < allItemsLength">
+                            <td
+                                v-intersect.quiet="onIntersectBottom"
+                                colspan="100%"
+                                align="center"
+                                :style="{ position: 'relative', height: 'auto' }"
+                            />
+                        </tr>
+                    </template> -->
+                </component>
+                <!-- старая реализация подгрузки по скроллу, по возможности убрать-->
+                <component
+                    :is="tableBody"
+                    v-else
+                    v-scroll:#scroll-container="onScrollControl"
+                    tag="tbody"
+                    v-bind="draggableProps"
+                    v-on="draggableListeners"
+                >
+                    <slot v-if="$slots['table-body']" name="table-body" />
+                    <!-- <template #footer>
+                        <tr v-if="loading">
+                            <td colspan="100%" align="center" :style="{ position: 'relative' }">
+                                <b-block-loader
+                                    position="static"
+                                    size="30"
+                                    style="max-height: 60px"
+                                />
+                            </td>
+                        </tr>
+                    </template>
+                    <template v-if="!draggable">
+                        <tr v-if="loading">
+                            <td colspan="100%" align="center" :style="{ position: 'relative' }">
+                                <b-block-loader
+                                    position="static"
+                                    size="30"
+                                    style="max-height: 60px"
+                                />
+                            </td>
+                        </tr>
+                    </template> -->
+                </component>
+            </template>
+        </v-simple-table>
+
+        <span v-if="loading" :style="{ position: 'relative' }">
+            <b-block-loader position="static" size="30" style="max-height: 60px" />
+        </span>
+
+        <span
+            v-else-if="nowItemsLength < allItemsLength"
+            v-intersect.quiet="onIntersectBottom"
+            colspan="100%"
+            align="center"
+            :style="{ position: 'relative', height: 'auto' }"
+        />
+    </div>
 </template>
 
 <script>
